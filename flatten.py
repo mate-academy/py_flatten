1008467bf226279a25a661b3605d715c929768f0
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-21 16:32:30 +0200

    Seems to be easy

--- flatten.py
+++ flatten.py
@@ -1,5 +1,14 @@
-from typing import List, Any
+"""module docstring"""
 
 
-def flatten(l: List[Any]) -> List[int]:
-    return []
+def flatten(*array):
+    """func docstring"""
+    a_string = "".join(str(array))
+    improved_arr = []
+    for seq, value in enumerate(a_string):
+        if value.isnumeric():
+            improved_arr += value
+    for seq, value in enumerate(improved_arr):
+        improved_arr[seq] = int(value)
+
+    return improved_arr
