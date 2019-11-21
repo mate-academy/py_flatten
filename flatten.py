1fb546728193e41f830ad1184467cc1c86f3436a
ValentinaLV <tina.lysenok@gmail.com>
2019-11-21 13:48:55 +0200

    hw4_py_flatten

--- flatten.py
+++ flatten.py
@@ -1,5 +1,16 @@
+"""
+flatten module
+"""
 from typing import List, Any
 
 
-def flatten(l: List[Any]) -> List[int]:
-    return []
+def flatten(lst: List[Any]) -> List[int]:
+    """
+    Flatten the nesting in an arbitrary list of values.
+    :param lst: List[Any]
+    :return: List[int]
+    """
+    for i, element in enumerate(lst):
+        while i < len(lst) and isinstance(lst[i], list):
+            lst[i:i + 1] = lst[i]
+    return lst
