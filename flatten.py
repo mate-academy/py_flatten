38a5ea1a5728c5d0e5cb66137b01d08f1e7d5b70
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-21 12:03:32 +0200

    v1

--- flatten.py
+++ flatten.py
@@ -1,5 +1,21 @@
+"""doc"""
 from typing import List, Any
 
 
-def flatten(l: List[Any]) -> List[int]:
-    return []
+def flatten(input_list: List[Any]) -> List[int]:
+    """doc"""
+    result = []
+
+    def remove_nested(lis):
+        """ loop through input list and checks type
+        of its elements. If element is nested list, call this
+         function again until element is not list, than append element
+         to resulting list"""
+        for element in lis:
+            if isinstance(element, list):
+                remove_nested(element)
+            else:
+                result.append(element)
+
+    remove_nested(input_list)
+    return result
