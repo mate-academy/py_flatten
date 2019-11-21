b1bb71f58574001ce6065a2d9696861e32aa28e8
alexander <earthhuman41@gmail.com>
2019-11-21 21:12:07 +0200

    implemented unpack of nested array

--- flatten.py
+++ flatten.py
@@ -1,5 +1,21 @@
+"""
+implemented unpack of nested array
+"""
 from typing import List, Any
 
 
-def flatten(l: List[Any]) -> List[int]:
-    return []
+def flatten(init_list: List[Any]) -> List[int]:
+    """
+
+    :param init_list: [] list of nested arrays
+    :return: [] unpacked array
+    """
+    result_list = []
+    for item in init_list:
+        if isinstance(item, list):
+            for sub_item in flatten(item):
+                result_list.append(sub_item)
+        else:
+            result_list.append(item)
+
+    return result_list
